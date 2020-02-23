959b07d4903dee0d9d606e04d0c9b3b9b3f45b09
Appoline <appoline.deangelis@gmail.com>
2020-02-23 15:18:56 +0100

    Mise en forme des recettes

--- recettes.php
+++ recettes.php
@@ -29,17 +29,48 @@ $req->bindParam(":pseudo", $_SESSION["pseudo"]);
 $req->execute();
 // 4. Afficher la recette
     while($result = $req->fetch(PDO::FETCH_ASSOC)){ // FETCH sert à parcourir chaque ligne et chaque colonne de la base de données
+
 ?>
                 
 <!-- Affichage des éléments de la recette -->
         <div class="recette">
-            <div class="pseudo"><?= $result["pseudo"] ?></div>
-            <div class="titre"><?= $result["titre"] ?></div>
-            <div class="ingredients"><?= $result["ingredients"] ?></div>
-            <div class="etapes"><?= $result["etapes"] ?></div>
-            <img src="<?= $result["image"] ?>" alt="US Cookies">
+            <!-- Partie textuelle de la recette -->
+            <div class="texte">
+                <div class="pseudo"><?= $result["pseudo"] ?></div>
+                <div class="titre"><?= $result["titre"] ?></div>
+                <div class="ingredients">
+                    <h2>Ingrédients</h2>
+                    <?= $result["ingredients"] = str_replace(",", "<br>", $result["ingredients"])?>
+                </div>
+                <div class="etapes">
+                    <h2>Étapes</h2>
+                    <?= $result["etapes"] = str_replace(".", "<br><br>", $result["etapes"])?>
+                </div>
+            </div>
+            <!-- Partie visuelle de la recette -->
+            <div class="image">
+                <img src="<?= $result["image"] ?>" alt="US Cookies">
+            </div>
         </div>
 
 <?php
     }
+?>
+
+<!-- Remplacer les "," par des retours à la ligne dans la recette -->
+<?php
+// $remplace = str_replace(",", "</br>", "etapes");
+// $remplace = str_replace(",", "\n", "etapes");
+
+
+// $req2 = $db->prepare("UPDATE recettes SET etapes = REPLACE (etapes, ',', '</br>') WHERE etapes LIKE '%,%'");
+
+// $remplacant="TEST";
+// $remplacer=",";
+// $remplace=str_replace($remplacant,$remplacer,$remplace);
+
+// $result = preg_replace(",","  TEST  ", "etapes");
+// $result=preg_replace("/\r/","\n", $result);
+// echo preg_replace(","," TEST  ", $result);
+
 ?>
